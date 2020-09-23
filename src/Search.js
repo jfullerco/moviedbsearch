9df15899afcf9c450c5c5c2977e7cbc7f5f7145e
jfullerco <jonathan@jfuller.co>
2020-09-22 23:18:22 -0400

    1 file modified

--- src/Search.js
+++ src/Search.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function Search() {
 
+  const [query, setQuery] = useState('');
+  
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+    
+    const url = `https://api.themoviedb.org/3/movie?api_key=d231347f0011ef23dea4aebf91515fa3&language=en-US&query=${query}&page=1&include_adult=false`
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json()
+      console.log(data)
+    }catch(err){ 
+      console.log(err) 
+    }
+
+  }
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={searchMovies}>
       <label htmlFor="query" className="Label">Movie Name</label>
       <input className="input" type="text" name="query" placeholder="ie. Jurassic Park" />
       <button className="button" type="submit">Submit</button>
